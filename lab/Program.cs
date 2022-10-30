7e04bc5e09bef730677a52b973f0c53b23f5e5d4
Dima <dimalebedev6902@gmail.com>
2022-10-30 21:20:20 +0200

    Initial Commit

---
+++ lab/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics.Contracts;
+
+namespace lab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<int> list = new MyList<int>();
+            list.Add(1);
+            list.Add(2);
+
+            foreach (int item in list.GetArray())
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine(MyClass<List<int>>.FactoryMethod());
+        }
+    }
+}
